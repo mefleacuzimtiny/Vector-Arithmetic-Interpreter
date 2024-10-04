f29484784a7d791f0ed813f8fa1808e72d36ced8
mefleacuzimtiny <arifu641@gmail.com>
2024-10-05 02:06:12 +0500

    Added parenthesis parser
    Added Vector structs
    Declared vector parsing funcs
    Downloaded phytolexer for learning and inspiration

--- Funcs.cpp
+++ Funcs.cpp
@@ -1,29 +1,54 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 using namespace std;
 
+// ignorables: '.'
+
+
+bool isDigit(char chr){
+	return (47 < chr) and (chr < 58);
+}
+
+bool isAlpha(char chr){
+	chr = tolower(chr);
+	return (96 < chr) and (chr < 123);
+}
+
+bool isFloat(string str){
+	int endofline = str.length();
+//	string strOfTokens = "";
+	char chr;
+	
+	for (int i = 0; i < endofline; i++) {
+		chr = str[i];
+		
+		while (isDigit(chr)) {
+			chr = str[++i]; 
+		}
+		if (not isDigit(chr) and (chr == '.')) {
+			chr = str[++i];
+			while (isDigit(chr)) {
+				chr = str[++i];
+			}
+		}
+		else {
+			cout << "INVALID SYNTAX" << endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+/* two issues: 
+	ints don't return true
+	invalid character at the very end does not return error
+*/
+
 int main(){
-    int input;
-    float calc, upper, lower, soosi;
-    cout << "num between 69 and 420? " << endl;
-    cin >> input;
-    lower = 69;
-    upper = 420;
-    
-    int diff_in = input - lower;
-    int diff_lim = upper - lower;
-    
-    soosi = (diff_in) / (diff_lim);
-    calc = (soosi + 1) - (soosi - ((diff_in) % (diff_lim)));
-    
-    cout << calc << endl;
-    
-    if (calc == 0){
-        cout << "True!" << endl;
-    }
-    else{
-        cout << "False!" << endl;
-    }
-    
-    return 0;
+	
+	cout << isFloat("2342") << endl;		// should be valid
+	
+	cout << isFloat("0.2342i") << endl;		// should be invalid
+	
+	return 0;
 }
